medmaha/mdm

Add: likes and unlike functionality

src/app/posts/components/PostCaption.tsx
@@ -26,7 +26,7 @@ export default function PostCaption({ caption, ...props }: Props) {
     <div
       className={`text-sm ${
         !showMediaName ? "flex" : ""
-      } items-center justify-between`}
+      } items-center justify-between px-2 py-2`}
     >
       {showMediaName && (
         <p className="pb-1">
@@ -40,7 +40,7 @@ export default function PostCaption({ caption, ...props }: Props) {
       >
         {caption}
       </p>
-      {!showMediaName && (
+      {!showMediaName && props.postType === "audio" && (
         <p className="text-sm opacity-70">
           <small>
             <time>{props.time}</time>

src/app/posts/components/PostCard.tsx
@@ -27,14 +27,14 @@ export default function Post({ post, user }: Props) {
       {post.fileType === "video" && <PostVideo post={post} />}
       {post.fileType === "audio" && <PostMusic post={post} />}
       {post.fileType === "other" && <p>Unknown file type!</p>}
-      <div className="space-y-4 p-2">
+      <div className="space-y-4">
         <PostCaption
           time={time}
           postType={post.fileType}
           caption={post.caption}
           mediaName={post.mediaName}
         />
-        <div className="flex items-center justify-between gap-3">
+        <div className="flex p-2 pb-0 items-center justify-between gap-3">
           {post.fileType !== "audio" && (
             <>
               <PostAuthor post={post} />

medmaha/brain-tr

src/app/posts/components/PostCaption.tsx
@@ -26,7 +26,7 @@ export default function PostCaption({ caption, ...props }: Props) {
     <div
       className={`text-sm ${
         !showMediaName ? "flex" : ""
-      } items-center justify-between`}
+      } items-center justify-between px-2 pt-2`}
     >
       {showMediaName && (
         <p className="pb-1">
@@ -40,7 +40,7 @@ export default function PostCaption({ caption, ...props }: Props) {
       >
         {caption}
       </p>
-      {!showMediaName && (
+      {!showMediaName && props.postType === "audio" && (
         <p className="text-sm opacity-70">
           <small>
             <time>{props.time}</time>

src/app/posts/components/PostCard.tsx
@@ -27,14 +27,14 @@ export default function Post({ post, user }: Props) {
       {post.fileType === "video" && <PostVideo post={post} />}
       {post.fileType === "audio" && <PostMusic post={post} />}
       {post.fileType === "other" && <p>Unknown file type!</p>}
-      <div className="space-y-4 p-2">
+      <div className="space-y-4">
         <PostCaption
           time={time}
           postType={post.fileType}
           caption={post.caption}
           mediaName={post.mediaName}
         />
-        <div className="flex items-center justify-between gap-3">
+        <div className="flex p-2 py-0 items-center justify-between gap-3">
           {post.fileType !== "audio" && (
             <>
               <PostAuthor post={post} />